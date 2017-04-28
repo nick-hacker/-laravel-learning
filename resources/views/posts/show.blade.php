5ccbb54e38a9d936abe45b35507d7ebf75880610
Nickamen <toto2ndu@gmail.com>
2017-04-29 02:17:42 +1400

    Show a single post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,7 +4,6 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            @foreach($posts as $post)
             <div class="panel panel-default">
                 <div class="panel-heading"><a href="/posts/{{ $post->id }}">{{ $post->title }}</a> by {{ $post->author->username }} {{ $post->created_at->diffForHumans() }}</div>
 
@@ -12,7 +11,6 @@
                     {{ $post->body }}
                 </div>
             </div>
-            @endforeach
         </div>
     </div>
 </div>
